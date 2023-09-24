b717e33e5688e108ac08955068552db5ceb01a48
RaziaAkbari <akbariharis14@gmail.com>
2023-09-24 17:42:48 +0430

    position of the map was changed

--- src/pages/Map/Map.jsx
+++ src/pages/Map/Map.jsx
@@ -4,18 +4,10 @@ import "leaflet/dist/leaflet.css"
 import "./Map.css"
 
 export default function Map() {
-  const [position, setPosition] = useState([51.505, -0.09])
+  // const [position, setPosition] = useState([51.505, -0.09])
 
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        setPosition([position.coords.latitude, position.coords.longitude])
-      },
-      (error) => {
-        console.error(error)
-      }
-    )
-  }, [])
+  //my position, retrieved from google maps
+  const position = [34.52604146664417, 69.1510697312263]
 
   return (
     <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
