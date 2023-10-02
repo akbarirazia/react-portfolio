8b925ce77390420b745a88dd48103b18f45b9298
RaziaAkbari <akbariharis14@gmail.com>
2023-10-02 18:58:43 +0430

    fixing 23

--- src/pages/What.jsx
+++ src/pages/What.jsx
@@ -11,29 +11,29 @@ import DropDown from "./components/FilterBar/DropDown"
 import img from "../assets/meme.jpg"
 
 function What() {
-  // const combinedData = [...medicalportfolio, ...projects]
-  // function shuffle(array) {
-  //   let currentIndex = array.length,
-  //     randomIndex
+  const combinedData = [...medicalportfolio, ...projects]
+  function shuffle(array) {
+    let currentIndex = array.length,
+      randomIndex
 
-  //   // While there remain elements to shuffle.
-  //   while (currentIndex > 0) {
-  //     // Pick a remaining element.
-  //     randomIndex = Math.floor(Math.random() * currentIndex)
-  //     currentIndex--
+    // While there remain elements to shuffle.
+    while (currentIndex > 0) {
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex)
+      currentIndex--
 
-  //     // And swap it with the current element.
-  //     ;[array[currentIndex], array[randomIndex]] = [
-  //       array[randomIndex],
-  //       array[currentIndex],
-  //     ]
-  //   }
+      // And swap it with the current element.
+      ;[array[currentIndex], array[randomIndex]] = [
+        array[randomIndex],
+        array[currentIndex],
+      ]
+    }
 
-  //   return array
-  // }
-  // shuffle(combinedData)
-  // const [array, setArray] = useState(combinedData)
-  const [array, setArray] = useState(medicalportfolio)
+    return array
+  }
+  shuffle(combinedData)
+  const [array, setArray] = useState(combinedData)
+  // const [array, setArray] = useState(medicalportfolio)
   // // console.log(combinedData)
   // // const [data, setData] = useState([])
   // // useEffect(() => {
