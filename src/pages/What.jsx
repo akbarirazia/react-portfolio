9d6c8994b5f8a0f9bb24042d10ad30a99155e5ea
RaziaAkbari <akbariharis14@gmail.com>
2023-10-02 16:58:14 +0430

    fixing

--- src/pages/What.jsx
+++ src/pages/What.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useMemo, useState } from "react"
 import Navbar from "./components/Navbar/Navbar"
-import { medicalportfolio } from "../Data/MedicalPort"
-import { projects } from "../Data/WebDev"
+// import { medicalportfolio } from "../Data/MedicalPort"
+// import { projects } from "../Data/WebDev"
 
 import "./What.css"
 
@@ -10,80 +10,80 @@ import Cards from "./components/Cards/Cards"
 import DropDown from "./components/FilterBar/DropDown"
 
 function What() {
-  const combinedData = [...medicalportfolio, ...projects]
-  function shuffle(array) {
-    let currentIndex = array.length,
-      randomIndex
+  // const combinedData = [...medicalportfolio, ...projects]
+  // function shuffle(array) {
+  //   let currentIndex = array.length,
+  //     randomIndex
 
-    // While there remain elements to shuffle.
-    while (currentIndex > 0) {
-      // Pick a remaining element.
-      randomIndex = Math.floor(Math.random() * currentIndex)
-      currentIndex--
+  //   // While there remain elements to shuffle.
+  //   while (currentIndex > 0) {
+  //     // Pick a remaining element.
+  //     randomIndex = Math.floor(Math.random() * currentIndex)
+  //     currentIndex--
 
-      // And swap it with the current element.
-      ;[array[currentIndex], array[randomIndex]] = [
-        array[randomIndex],
-        array[currentIndex],
-      ]
-    }
+  //     // And swap it with the current element.
+  //     ;[array[currentIndex], array[randomIndex]] = [
+  //       array[randomIndex],
+  //       array[currentIndex],
+  //     ]
+  //   }
 
-    return array
-  }
-  shuffle(combinedData)
-  const [array, setArray] = useState(combinedData)
-  // console.log(combinedData)
-  // const [data, setData] = useState([])
-  // useEffect(() => {
-  //   fetch("https://api.github.com/users/akbarirazia/repos")
-  //     .then((response) => response.json())
-  //     .then((data) => setData(data))
-  // }, [])
+  //   return array
+  // }
+  // shuffle(combinedData)
+  // const [array, setArray] = useState(combinedData)
+  // // console.log(combinedData)
+  // // const [data, setData] = useState([])
+  // // useEffect(() => {
+  // //   fetch("https://api.github.com/users/akbarirazia/repos")
+  // //     .then((response) => response.json())
+  // //     .then((data) => setData(data))
+  // // }, [])
 
-  // console.log(data)
-  function handleFilter(option) {
-    // console.log(option)
-    if (option === "All Portfolios") {
-      setArray(combinedData)
-    } else if (option === "Web Development") {
-      setArray(projects)
-    } else {
-      setArray(medicalportfolio)
-    }
-  }
-  const [query, setQuery] = useState("")
+  // // console.log(data)
+  // function handleFilter(option) {
+  //   // console.log(option)
+  //   if (option === "All Portfolios") {
+  //     setArray(combinedData)
+  //   } else if (option === "Web Development") {
+  //     setArray(projects)
+  //   } else {
+  //     setArray(medicalportfolio)
+  //   }
+  // }
+  // const [query, setQuery] = useState("")
 
-  const filteredProjects = useMemo(
-    () =>
-      array.filter((project) =>
-        project.title.toLowerCase().includes(query.toLowerCase())
-      ),
-    [array, query]
-  )
-  function handleChange(e) {
-    console.log(e.target.value)
-    // setArray((prev) => {
-    //   return prev.filter((arr) =>
-    //     arr.title.toLowerCase().includes(e.target.value)
-    //   )
-    // })
-  }
-  const finalProject = filteredProjects.map((data, index) => (
-    <Cards
-      key={index}
-      title={data.title}
-      description={data.description}
-      link={data.link}
-      // img={data.imageSrc}
-      tags={data.tags}
-      delay={index}
-    />
-  ))
-  console.log(finalProject.length)
+  // const filteredProjects = useMemo(
+  //   () =>
+  //     array.filter((project) =>
+  //       project.title.toLowerCase().includes(query.toLowerCase())
+  //     ),
+  //   [array, query]
+  // )
+  // function handleChange(e) {
+  //   console.log(e.target.value)
+  //   // setArray((prev) => {
+  //   //   return prev.filter((arr) =>
+  //   //     arr.title.toLowerCase().includes(e.target.value)
+  //   //   )
+  //   // })
+  // }
+  // const finalProject = filteredProjects.map((data, index) => (
+  //   <Cards
+  //     key={index}
+  //     title={data.title}
+  //     description={data.description}
+  //     link={data.link}
+  //     img={data.imageSrc}
+  //     tags={data.tags}
+  //     delay={index}
+  //   />
+  // ))
+  // console.log(finalProject.length)
   return (
     <div>
       <Navbar />
-      <div className="container">
+      {/* <div className="container">
         <div
           style={{
             display: "flex",
@@ -97,7 +97,7 @@ function What() {
           <SearchBar onchange={(e) => setQuery(e.target.value)} value={query} />
           <DropDown onFilter={handleFilter} />
         </div>
-        {/* <Cards /> */}
+        
         <div
           className="flex flex-wrap gap-2x card-container"
           style={{ justifyContent: "space-evenly" }}
@@ -111,7 +111,7 @@ function What() {
       </div>
       <br />
       <br />
-      <br />
+      <br /> */}
     </div>
   )
 }
