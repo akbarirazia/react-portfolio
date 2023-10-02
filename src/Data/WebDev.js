0fc9c5e502f5f2178c71bf5eb6edd9dd54b525dc
RaziaAkbari <akbariharis14@gmail.com>
2023-10-02 18:38:01 +0430

    fixing 19

--- src/Data/WebDev.js
+++ src/Data/WebDev.js
@@ -1,14 +1,14 @@
-import portfolio from "../assets/port.jpg"
-import game from "../assets/game.jpg"
-import invoice from "../assets/invoice.jpg"
-import album from "../assets/album.jpg"
-import bookstore from "../assets/bookstore.jpg"
-import AyKash from "../assets/AyKash.jpg"
+// import portfolio from "../assets/port.jpg"
+// import game from "../assets/game.jpg"
+// import invoice from "../assets/invoice.jpg"
+// import album from "../assets/album.jpg"
+// import bookstore from "../assets/bookstore.jpg"
+// import AyKash from "../assets/AyKash.jpg"
 
 export const projects = [
   {
     title: "Ay Kash -Under The Starry Afghan Sky",
-    imageSrc: AyKash,
+    imageSrc: "../assets/port.jpg",
     description:
       "‘Ay Kash / ای کاش / If Only’ is a theatre, art and activism performance / exhibition featuring the artwork, films, poetry and stories of eleven young women based Afghanistan coming to Berlin, Germany in September 2023 and then available to tour worldwide. A project initiated by the Cosmino Productions.",
     link: "http://www.underthestarryafghansky.com/",
@@ -16,7 +16,7 @@ export const projects = [
   },
   {
     title: "Invoice App",
-    imageSrc: invoice,
+    imageSrc: "../assets/port.jpg",
     description:
       "An invoice is a commercial document issued by a seller to a buyer that itemizes the products, services, or goods provided, along with their respective quantities and prices. It serves as a formal request for payment for the goods or services rendered. This project was created using bootstrap for its responsive design and JavaScript for its functionalities.",
     link: "https://akbarirazia.github.io/invoice-app/",
@@ -24,7 +24,7 @@ export const projects = [
   },
   {
     title: "Bookstore Landing Page",
-    imageSrc: bookstore,
+    imageSrc: "../assets/port.jpg",
     description:
       "An elegant and responsive bookstore landing page, created using Tailwind CSS. This static project brings a sophisticated touch to book lovers worldwide. With its visually pleasing design, optimized for all screen sizes. The combination of Tailwind CSS's sleek styling and the static nature of the website ensures fast loading times and a seamless browsing experience.",
     link: "https://akbarirazia.github.io/Bookstore-Landing-Page/",
@@ -32,7 +32,7 @@ export const projects = [
   },
   {
     title: "Personal Portfolio",
-    imageSrc: portfolio,
+    imageSrc: "../assets/port.jpg",
     description:
       "My personal portfolio! the page you are at right now. This project was created using tailwindcss for its responsive design and JavaScript for the certain functionalities in the page. It contains my personal portfolio and contact information along with my resume.",
     link: "https://akbarirazia.github.io/portfolio/",
@@ -40,7 +40,7 @@ export const projects = [
   },
   {
     title: "Responsive Album",
-    imageSrc: album,
+    imageSrc: "../assets/port.jpg",
     description:
       "A responsive static album with Bootstrap is a web page that showcases a collection of images or media in an organized and visually appealing manner. It is built using the Bootstrap framework, which provides a set of pre-designed CSS and JavaScript components for creating responsive and mobile-friendly websites.",
     link: "https://akbarirazia.github.io/responsive-album-bootstrap/",
@@ -48,7 +48,7 @@ export const projects = [
   },
   {
     title: "Rock, Paper, Scissors Game",
-    imageSrc: game,
+    imageSrc: "../assets/port.jpg",
     description:
       "Rock, Paper, Scissors Game: A JavaScript-based game where players compete against the computer by choosing between rock, paper, or scissors. Test your strategy and luck in this classic hand gesture showdown!",
     link: "https://3w17p8.csb.app/",
