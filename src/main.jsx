457acb5c802d3163aea135fa510601a928ffd201
RaziaAkbari <akbariharis14@gmail.com>
2023-11-29 17:38:43 +0430

    loader added for translator to load

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,18 @@ import { createRoot } from "react-dom/client"
 import "./styles.scss"
 import App from "./App"
 import "./i18n"
+import { CircularProgress } from "@mui/material"
 const rootElement = document.getElementById("root")
 const root = createRoot(rootElement)
 root.render(
   <StrictMode>
-    <Suspense fallback="loading">
+    <Suspense
+      fallback={
+        <div className="center">
+          <CircularProgress />
+        </div>
+      }
+    >
       <App />
     </Suspense>
   </StrictMode>
